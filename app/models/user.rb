38b39c523486d9a54dcaba4d5d55a93d87928f7d
Gaurav Aradhye <gaurav.aradhye@clogeny.com>
2015-09-20 02:35:46 -0400

    Adding association between user and book model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :books
   validates :name, presence => true, format => {:with => /[a-z]/}
   validates :email, presence => true, uniqueness => true, email_format: {message: "email id format is not valid"}
   validates :password, presence => true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150920041229) do
+ActiveRecord::Schema.define(version: 20150920055512) do
 
   create_table "books", force: :cascade do |t|
     t.text     "description"
@@ -20,7 +20,10 @@ ActiveRecord::Schema.define(version: 20150920041229) do
     t.boolean  "is_requested"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "user_id"
   end
+
+  add_index "books", ["user_id"], name: "index_books_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
