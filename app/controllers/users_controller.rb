cfe34540fb4a20a190fb1d5c4a8faa7ace6e488c
Gaurav Aradhye <gaurav.aradhye@clogeny.com>
2015-09-20 00:46:20 -0400

    Adding Users controller and related methods

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def show
+  end
+
+  def add
+  end
+
+  def delete
+  end
+end
---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionController::TestCase
+  test "should get show" do
+    get :show
+    assert_response :success
+  end
+
+  test "should get add" do
+    get :add
+    assert_response :success
+  end
+
+  test "should get delete" do
+    get :delete
+    assert_response :success
+  end
+
+end
