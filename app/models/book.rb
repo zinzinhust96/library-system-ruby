3636814e43e411b5ff9c0a373568dd44d80f37eb
Gaurav Aradhye <gaurav.aradhye@clogeny.com>
2015-09-23 03:56:53 -0400

    Adding validation to Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,8 @@
 class Book < ActiveRecord::Base
+  validates :title,
+            :presence => true
+  validates :isbn,
+            :presence => true
+  validates :description,
+            :presence => true
 end
