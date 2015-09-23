56accf6e406f2a03c20cead734b06f90b60410f7
Gaurav Aradhye <gaurav.aradhye@clogeny.com>
2015-09-23 15:33:12 -0400

    Adding basic login mechanism using session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      redirect_to "/users"
+      log_in user
+      redirect_to user_path(user.id)
     else 
       flash.now[:danger] = "Invalid email id or password. Please try again."
       render 'new'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,6 @@
 module SessionsHelper
+  #This method logs in the user
+  def log_in(user)
+    session[:user_id] = user.id
+  end
 end
