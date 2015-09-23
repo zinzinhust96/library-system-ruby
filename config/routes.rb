ba099e87400b8d0c02d6b08397fd3bcd71900505
Gaurav Aradhye <gaurav.aradhye@clogeny.com>
2015-09-23 15:32:30 -0400

    Changing index page to login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root :to => 'sessions#new'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
