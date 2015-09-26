bab50a3d62a2731bc11ef06f2c2cc2dc900c753d
Gaurav Aradhye <gaurav.aradhye@clogeny.com>
2015-09-26 15:00:00 -0400

    Changing email subject

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -5,6 +5,6 @@ class Notifier < ApplicationMailer
   def send_notification(email, user_name, book_name)
     @book_name = book_name
     @user_name = user_name
-    mail(to: email)
+    mail(to: email, subject: "Book #{book_name} is now available!")
   end
 end
