febd646aca30edc4814a7ea59dec6f45b6286f2d
Doan Ngoc Nam <nam.dng2511@gmail.com>
2018-03-25 21:18:58 +0700

    second commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150926194133) do
+ActiveRecord::Schema.define(version: 20180326194133) do
 
   create_table "book_histories", force: :cascade do |t|
     t.integer "book_id"
